1d7195739bf2df3f0729c4278e9a6770a6df333b
Oskars Germovs <oskars_germovs@inbox.lv>
2024-09-19 00:00:48 +0300

    Hello Friend

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿namespace HelloFriend
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, friend. Hello, friend? That’s lame. Maybe I should give you a name, but that’s a slippery slope. You’re only in my head. We have to remember that. Shit.!");
+        }
+    }
+}
